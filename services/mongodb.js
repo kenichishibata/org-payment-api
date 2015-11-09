11a7135afa208b2215a02161cae74e5c0bff3dad
Kenichi Shibata <kenichi.shibata@fastretailing.com>
2015-11-09 09:44:40 +0900

    added basic put

--- services/mongodb.js
+++ services/mongodb.js
@@ -139,6 +139,7 @@ function putToTableHead(req,res){
           res.writeHead(200, {'Content-Type' : 'application/json; charset=utf-8'});
           res.end(JSON.stringify(updProd));
       });
+    });  
 }
 //
 // function putToTableBody(){
