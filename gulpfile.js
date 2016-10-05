1c3b9fad670228010aec88a860c4a8c47b11d4d8
kenichishibata <kenichi.shibata3123@gmail.com>
2016-10-05 17:42:54 +0900

    added helpers

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var jshint = require('gulp-jshint');
-var gulp   = require('gulp');
-
-gulp.task('lint', function() {
-  return gulp.src(['./*.js','routes/*.js','config/*.js','controller/*.js','libs/*.js','services/*.js'])
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
-});
